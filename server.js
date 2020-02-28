df6d831a85989f93e7eb330ea0a56b982e0601e5
Hemanth Babu Parupalli <hemanthbabuparupalli@Hemanths-MacBook-Pro.local>
2020-02-27 20:31:59 -0400

    started working on DevCamper

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const dotenv  = require('dotenv')
+
+// Load env variables
+
+dotenv.config({ path:'./config/config.env' }) 
+
+
+const app = express()
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`App is running in ${NODE_ENV} on port ${PORT}`))
